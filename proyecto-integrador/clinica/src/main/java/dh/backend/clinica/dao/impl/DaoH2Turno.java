4fc3d31fd1ce4ebb063b602062f3d02bad68cb81
laurasot <lauraflorencia5@gmail.com>
2024-08-22 13:11:17 -0400

    subiendo avances de turno

---
+++ proyecto-integrador/clinica/src/main/java/dh/backend/clinica/dao/impl/DaoH2Turno.java
@@ -0,0 +1,23 @@
+package dh.backend.clinica.dao.impl;
+
+import dh.backend.clinica.dao.IDao;
+import dh.backend.clinica.model.Turno;
+
+import java.util.List;
+
+public class DaoH2Turno implements IDao<Turno> {
+    @Override
+    public Turno guardar(Turno turno) {
+        return null;
+    }
+
+    @Override
+    public Turno buscarPorId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Turno> listaTodos() {
+        return null;
+    }
+}
