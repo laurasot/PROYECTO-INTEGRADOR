cd16c3e4a9e8a457e3115e61998995f6fa67b57a
laurasot <lauraflorencia5@gmail.com>
2024-09-01 16:36:58 -0400

    probando todo, funciona perfecto

--- clinica/src/main/java/dh/backend/clinica/controller/OdontologoController.java
+++ clinica/src/main/java/dh/backend/clinica/controller/OdontologoController.java
@@ -1,9 +1,14 @@
 package dh.backend.clinica.controller;
 
+import dh.backend.clinica.model.Odontologo;
 import dh.backend.clinica.service.impl.OdontologoService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/odontologos")
 public class OdontologoController {
 
     private OdontologoService odontologoService;
@@ -12,5 +17,19 @@ public class OdontologoController {
         this.odontologoService = odontologoService;
     }
 
+    @PostMapping("/guardar")
+    public ResponseEntity<Odontologo> agregarOdontologo(@RequestBody Odontologo odontologo){
+        // aca jackson convierte el objeto JSON a un objeto Java "paciente"
+        return ResponseEntity.ok(odontologoService.guardarOdontologo(odontologo));
+    }
 
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Odontologo> buscarPorId(@PathVariable Integer id){
+        Optional<Odontologo> odontologo = odontologoService.buscarPorId(id);
+        if(odontologo.isPresent()){
+            return ResponseEntity.ok(odontologo.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- clinica/src/main/java/dh/backend/clinica/controller/TurnoController.java
+++ clinica/src/main/java/dh/backend/clinica/controller/TurnoController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/turno")
+@RequestMapping("/turnos")
 public class TurnoController {
 
     private TurnoService turnoService;
